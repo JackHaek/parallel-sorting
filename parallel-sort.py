188435e241141d832317f4b2824414162ef436fd
JackHaek <jackjhaek@gmail.com>
2023-10-16 18:56:51 -0500

    add threaded merge sort

--- parallel-sort.py
+++ parallel-sort.py
@@ -56,6 +56,46 @@ def merge_sort_single_thread(arr):
             j += 1
             k += 1
 
+def merge(arr, low, mid, high):
+    left = arr[low:mid+1]
+    right = arr[mid+1:high+1]
+
+    i = j = 0
+    k = low
+
+    while i < len(left) and j < len(right):
+        if left[i] <= right[j]:
+            arr[k] = left[i]
+            i += 1 
+        else:
+            arr[k] = right[j]
+            j += 1
+        k += 1
+
+    while i < len(left):
+        arr[k] = left[i]
+        i += 1
+        k += 1
+
+    while j < len(right):
+        arr[k] = right[j]
+        j += 1
+        k += 1
+
+
+def merge_sort(arr, low, high):
+    if low < high:
+        mid = low + (high - low) // 2
+
+        merge_sort(low, mid)
+        merge_sort(mid + 1, high)
+
+        merge(arr, low, mid, high)
+
+def merge_sort_threaded(arr):
+    merge_sort(arr, 0, len(arr) - 1)
+
+
 if __name__ == '__main__':
     processes = multiprocessing.cpu_count()
     print('Processes: ', processes)
@@ -69,4 +109,8 @@ if __name__ == '__main__':
 
     myArray = toCopy.copy()
     merge_sort_single_thread(myArray)
+    print(myArray)
+
+    myArray = toCopy.copy()
+    p.map(merge_sort_threaded, myArray)
     print(myArray)
